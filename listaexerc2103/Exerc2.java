22dd863ad0e463fc43ed110dee02e183643d9497
Miriã Rocha Brizola <117657428+miriaBr7@users.noreply.github.com>
2023-03-21 10:42:13 -0300

    Add files via upload

---
+++ listaexerc2103/Exerc2.java
@@ -0,0 +1,18 @@
+package listaexerc2103;
+
+public class Exerc2 {
+
+	public static void main(String[] args) {
+		// exercicio 3//
+		
+		int n = 5;
+		int r= 3;
+		
+		for(int n1=0; n1<10; n1++) {
+		
+			System.out.println(n);
+			n=n+r;
+		}
+	}
+}
+
---
+++ listaexerc2103/exerc1.java
@@ -0,0 +1,14 @@
+package listaexerc2103;
+
+public class exerc1 {
+
+	public static void main(String[] args) {
+		//resposta com for//
+		
+		for(int i=1; i<=19; i=i+2) {
+		System.out.println(i);
+		
+		}
+	}
+
+}
